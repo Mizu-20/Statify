3028f508cc1350fd12b469a4c57c81dba1363131
Mizu-20 <dhyey.rdr@gmail.com>
2025-04-07 02:58:05 +0530

    Update routes.ts
    let and const stuff

--- server/routes.ts
+++ server/routes.ts
@@ -59,10 +59,8 @@ export async function registerRoutes(app: Express): Promise<Server> {
 
     // Get Replit-specific URL
     // For Spotify OAuth to work, this URL must be EXACTLY the same as registered in Spotify Developer Dashboard
-    const redirectUri = process.env.SPOTIFY_REDIRECT_URI!;
+    let redirectUri = process.env.SPOTIFY_REDIRECT_URI!;
     console.log("Using redirect URI:", redirectUri);
-    
-    let redirectUri;
 
     redirectUri = process.env.SPOTIFY_REDIRECT_URI!;
     console.log("Using redirect URI:", redirectUri);
